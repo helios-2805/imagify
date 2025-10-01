55561445eaf32a69c61057cd89de40f6129afba9
Surya Sai <saisuryapalthi@gmail.com>
2025-10-01 22:01:59 +0530

    Add image generation functionality with user credit validation

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 // importing the mongodb config file
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/user.routes.js'
+import imageRouter from './routes/image.routes.js'
 
 dotenv.config({
   path: './.env'
@@ -20,6 +21,8 @@ app.use(cors())
 await connectDB()
 
 app.use('/api/v1/user', userRouter)
+app.use('/api/v1/image', imageRouter)
+
 
 app.get('/', (req, res)=> {
   res.status(200).send('API Working!')
